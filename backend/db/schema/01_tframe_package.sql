efae3f65f8bbb99b738f9532f033aec85d49f85a
Kenniku Tolato <kenniku_you@ymail.com>
2026-02-27 08:10:49 -0700

    Init db schemas and structure.

---
+++ backend/db/schema/01_tframe_package.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tframe_package (
+  tf_package_id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  frame_type VARCHAR(255),
+  supplier VARCHAR(255),
+  part_qty INTEGER,
+  photo text,
+  qr_code_photo text,
+  status VARCHAR(255),
+  last_scanned_by INTEGER
+);
+
+CREATE TABLE parts_list (
+  part_uid VARCHAR(255) PRIMARY KEY,
+  tf_package_id references tframe_package(tf_package_id) ON DELETE CASCADE,
+  description text default 'basic part',
+  qr_code_photo text,
+  last_scanned_by INTEGER,
+  status VARCHAR(255)
+)
