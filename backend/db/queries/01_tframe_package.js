e029aaabc16cff23b399cd23f3570883eb6fee40
Kenniku Tolato <kenniku_you@ymail.com>
2026-02-27 21:32:36 -0700

    Init tframe get by id query.

---
+++ backend/db/queries/01_tframe_package.js
@@ -0,0 +1,16 @@
+const db = require('../connection')l;
+
+
+//Get tframe data by tfframe package id
+const getTframePkgbyID =  (tframeData) {
+  const query = `SELECT * FROM tframe_package WHERE id = $1`;
+  const data = [tframeData];
+
+
+  return db.query(query, values).then((data) => {
+    console.log("db-query: ", data.rows);
+    return data.rows
+  })
+
+
+}
